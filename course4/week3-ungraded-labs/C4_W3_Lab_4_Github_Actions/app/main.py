f69a00fe758647284fe4ea734cc7f4a5496497e8
Diego Arcos <diego.arcos@nunsys.com>
2024-04-29 20:27:32 +0200

    Testing the CI/CD

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel, conlist
 
 
-
+# Cambios github
 app = FastAPI(title="Predicting Wine Class with batching")
 
 # Open classifier in global scope
